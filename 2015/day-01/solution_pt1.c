40ad7a33ba4893778ee05ef4797541754e0f7beb
Florian-Katerndahl <florian@katerndahl.com>
2022-11-01 22:33:46 +0100

    day 1 for 2015 and upadte readme

---
+++ 2015/day-01/solution_pt1.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+void read_directions_from_file(FILE *file, char **buff, size_t *inital_buffer);
+int parse_directions(char *direction);
+
+int main(int argc, char *argv[]) {
+
+	assert(argc == 2);
+
+	size_t buffer_size = 2000;
+
+	char *directions = calloc(buffer_size, sizeof(char));
+
+	FILE *f_input = fopen(*(argv + 1), "rt");
+
+	if (f_input == NULL) {
+		fprintf(stderr, "Could not open %s\n", *(argv + 1));
+		exit(1);
+	}
+
+	read_directions_from_file(f_input, &directions, &buffer_size);
+
+	int floor_level = parse_directions(directions);
+
+	printf("Santa ends up on floor N° %d\n", floor_level);
+
+	fclose(f_input);
+	free(directions);
+	return 0;
+}
+
+void read_directions_from_file(FILE *file, char **buff, size_t *inital_buffer) {
+	int nread = 0;
+	if (( nread = getline(buff, inital_buffer, file)) == -1) {
+		fprintf(stderr, "Failed to read string");
+		exit(1);
+	}
+
+	(*buff)[*inital_buffer - 1] = '\0';
+}
+
+int parse_directions(char *direction) {
+	int starting_point = 0;
+	char *p = direction;
+	while (*p) {
+		switch (*p) {
+			case '(':
+				starting_point++;
+				break;
+			case ')':
+				starting_point--;
+				break;
+			default:
+				break;
+		}
+		p++;
+	}
+	return starting_point;
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# advent-of-code
+# Advent of Code
+
+This repo contains my attempts at solving the puzzles presented in the (Advent of Code)[https://adventofcode.com/].
+There is not really an more to say than that, apart from: The chance of this getting completet ever is fairly slim
+and I'll see how long I'm actually motivated on working on this project.
+
+## Setup
+
+If, for whatever reason, you want to clone and set up this repository for yourself, you need:
+
+1. You need a C compiler (e.g. `gcc`)
+2. 
+
+```bash
+git clone https://github.com/Florian-Katerndhal/advent-of-code.git
+cd advent-of-code
+```
+
