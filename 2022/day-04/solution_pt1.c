8f0abc4ca6b241b7af7c3ad554b9dd0ed46ab800
Florian-Katerndahl <florian@katerndahl.com>
2022-12-15 20:54:09 +0100

    add day 4 of AoC 2022

---
+++ 2022/day-04/solution_pt1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define LINE 100
+
+struct Elf {
+    int start_id;
+    int end_id;
+};
+
+struct Pair {
+    struct Elf first;
+    struct Elf second;
+};
+
+unsigned long check_for_complete_overlap(struct Pair *pair);
+
+int main(int argc, char *argv[]) {
+  assert(argc == 2);
+
+  char line[LINE];
+  struct Pair pair_of_elves;
+  unsigned long complete_overlap = 0;
+
+  FILE *f = fopen(*(argv + 1), "rt");
+
+  if (f == NULL) {
+    fprintf(stderr, "Failed to open file %s\n", *(argv + 1));
+    exit(1);
+  }
+
+  while (!feof(f)) {
+    if (fgets(line, LINE, f) == NULL)
+      break;
+
+    if (sscanf(line, "%d-%d,%d-%d\n", &pair_of_elves.first.start_id, &pair_of_elves.first.end_id,
+               &pair_of_elves.second.start_id, &pair_of_elves.second.end_id) != 4)
+      break;
+
+    complete_overlap += check_for_complete_overlap(&pair_of_elves);
+  }
+
+  printf("Number of pairs of elves where one assigned range is complete within the other: %ld\n",
+         complete_overlap);
+  return 0;
+}
+
+unsigned long check_for_complete_overlap(struct Pair *pair) {
+  return (unsigned long) ((pair->first.start_id <= pair->second.start_id && pair->first.end_id >= pair->second.end_id)
+                          ||
+                          (pair->second.start_id <= pair->first.start_id && pair->second.end_id >= pair->first.end_id));
+}
